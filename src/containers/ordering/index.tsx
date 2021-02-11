deb97795e8a0d20221b4705a39a806e1868567cd
Christina Yuneva <christina_yuneva@mail.ru>
2021-02-11 11:47:47 +0400

    rp-7 | Вёрстка странички оформления заказа, заменила текстовки

--- src/containers/ordering/index.tsx
+++ src/containers/ordering/index.tsx
@@ -4,6 +4,7 @@ import style from './style.css';
 import asset from '../../asset';
 import { URLs } from '../../__data__/urls';
 import { useTranslation } from 'react-i18next';
+import { text } from 'express';
 
 function Order() {
     const { t, i18n } = useTranslation();
@@ -11,25 +12,32 @@ function Order() {
         <div className={style.wrapper}>
             <header className={style.header}>
                 <Link className={style.name} to={URLs.home.url}>
-                    FLEUR
+                    {t('repos.name')}
                 </Link>
             </header>
             <div className={style.order}>
-                <h1>Оформление заказа</h1>
+                <h1>{t('repos.ordering')}</h1>
                 <div className={style.form}>
-                    <div>
-                        <label>Введите ююю</label>
+                    <div className={style.input}>
+                        <label>{t('repos.input_name')}</label>
                         <input />
                     </div>
-                    <div>
-                        <label>Введите ююю</label>
+                    <div className={style.input}>
+                        <label>{t('repos.input_surname')}</label>
                         <input />
                     </div>
-                    <div>
-                        <label>Введите ююю</label>
+                    <div className={style.input}>
+                        <label>{t('repos.input_adress')}</label>
                         <input />
                     </div>
-                    <input className={style.line} type="text" />
+                    <div className={style.input}>
+                        <label>{t('repos.input_index')}</label>
+                        <input />
+                    </div>
+                    <div></div>
+                    <button className={style.arrange}>
+                        {t('repos.arrange')}
+                    </button>
                 </div>
             </div>
         </div>
